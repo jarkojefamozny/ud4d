ca9b787004df0171cfdae19fc5411011e84da449
jarkojefamozny <jaroslavbonco@gmail.com>
2022-08-17 22:39:51 +0200

    Unification

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,26 @@ import Login from './Login';
 function App() {
   return (
     <>
-      <header className="App-header">
+      <header className='App-header'>
         UD4D login test
       </header>
       <div className='content'>
         <div className='assignment'>
-          Zadanie: 
+          Zadanie:
           <p>V TypeScriptu pomocí frameworku React vytvořte uživatelské rozhraní pro přihlašování. Tento
-          formulář bude obsahovat 2 vstupní inputy (uživatelské jméno a heslo). Formulář bude kontrolovat
-          správnost uživatelských údajů již na klientovi (tzv. není potřeba odesílat na backend). Validní
-          uživatelské jméno může obsahovat pouze malá písmena bez diakritiky, číslice 0 až 9, tečku, pomlčku
-          a musí obsahovat právě jeden zavináč. Minimální délka uživatelského jména před zavináčem musí
-          být 5 znaků. Minimální délka hesla je 6 znaků.
-          Po odeslání formuláře (použijte RESTful API nebo GraphQL) se uživateli zobrazí, zda se mu podařilo
-          přihlásit či nikoliv. Pro tyto účely si můžete udělat jednoduchý backend, mock backend nebo
-          libovolné jiné řešení. Stačí mít natvrdo nadefinovaná správná jména a hesla (není potřeba vytvářet
-          žádnou databázi).
-          Vzhled formuláře, způsob kontroly a styl zápisu kódu je čistě na Vás. Doporučujeme použít knihovnu
-          Material UI.
-          Pokud by něco v zadání nebylo jasné, tak to můžete popsat v README spolu s variantou, kterou jste
-          nakonec implementovali.
+            formulář bude obsahovat 2 vstupní inputy (uživatelské jméno a heslo). Formulář bude kontrolovat
+            správnost uživatelských údajů již na klientovi (tzv. není potřeba odesílat na backend). Validní
+            uživatelské jméno může obsahovat pouze malá písmena bez diakritiky, číslice 0 až 9, tečku, pomlčku
+            a musí obsahovat právě jeden zavináč. Minimální délka uživatelského jména před zavináčem musí
+            být 5 znaků. Minimální délka hesla je 6 znaků.
+            Po odeslání formuláře (použijte RESTful API nebo GraphQL) se uživateli zobrazí, zda se mu podařilo
+            přihlásit či nikoliv. Pro tyto účely si můžete udělat jednoduchý backend, mock backend nebo
+            libovolné jiné řešení. Stačí mít natvrdo nadefinovaná správná jména a hesla (není potřeba vytvářet
+            žádnou databázi).
+            Vzhled formuláře, způsob kontroly a styl zápisu kódu je čistě na Vás. Doporučujeme použít knihovnu
+            Material UI.
+            Pokud by něco v zadání nebylo jasné, tak to můžete popsat v README spolu s variantou, kterou jste
+            nakonec implementovali.
           </p>
           <p> Ukázka validního uživatelského jména:</p>
           <ul>
@@ -37,12 +37,12 @@ function App() {
             <li>Nevalidnijmeno</li>
           </ul>
           <p>
-          Pro odevzdání použijte GitHub/Bitbucket/Gitlab (poslat nám link na daný repozitář).
+            Pro odevzdání použijte GitHub/Bitbucket/Gitlab (poslat nám link na daný repozitář).
           </p>
         </div>
         <div className='solution'>
           Riesenie:
-          <Login/>
+          <Login />
         </div>
       </div>
     </>
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,30 +1,30 @@
-import { Box, Button, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import { Box, Button, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import SendIcon from '@mui/icons-material/Send';
 
-import { Component } from "react";
+import { Component } from 'react';
 
-interface IProps {}
+interface IProps { }
 
 interface IState {
-  login: string;
-  password: string;
-  showPassword: boolean;
+    login: string;
+    password: string;
+    showPassword: boolean;
 }
 
-class Login extends Component<IProps,  IState> {
+class Login extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
         this.state = {
-            login: "",
-            password: "",
+            login: '',
+            password: '',
             showPassword: false,
         };
     }
 
     handleClickShowPassword = () => {
-        this.setState({showPassword: !this.state.showPassword})
+        this.setState({ showPassword: !this.state.showPassword })
     };
 
     /*
@@ -33,7 +33,7 @@ class Login extends Component<IProps,  IState> {
             - part before @ must have at least 5 chars
             - part after @ whatever from allowed chars
      */
-    isLoginValid(login : string) {
+    isLoginValid(login: string) {
         const regex = new RegExp('^[a-z0-9.-]{5,}@[a-z-.0-9]*$')
         return regex.test(login)
     }
@@ -42,20 +42,20 @@ class Login extends Component<IProps,  IState> {
         Valid password:
             - length must be at least 5 chars
      */
-    isPasswordValid(password : string) {
+    isPasswordValid(password: string) {
         return password.length > 5
     }
 
     getError(value: string, type: string) {
-        if(!(type === 'login' ? this.isLoginValid(value) : this.isPasswordValid(value)) && value.length !== 0) return "Incorrect " + type + ". Please read instructions."
-        else return ""
+        if (!(type === 'login' ? this.isLoginValid(value) : this.isPasswordValid(value)) && value.length !== 0) return 'Incorrect ' + type + '. Please read instructions.'
+        else return ''
     }
 
     getColor(value: string, type: string) {
-        if(value.length === 0) {
+        if (value.length === 0) {
             return undefined
         }
-        else return (type === 'login' ? this.isLoginValid(value) : this.isPasswordValid(value)) ? "success" : "error"
+        else return (type === 'login' ? this.isLoginValid(value) : this.isPasswordValid(value)) ? 'success' : 'error'
     }
 
     isDataValid(login: string, password: string) {
@@ -69,77 +69,77 @@ class Login extends Component<IProps,  IState> {
                 'Content-Type': 'application/json',
             },
             credentials: 'include',
-            body: JSON.stringify({login: this.state.login, password: this.state.password}),
-            })
-            .then((response) => {
-                // faking a happy response
-                alert(this.isDataValid(this.state.login, this.state.password) ? "Success! Congratulations" : "Mission Failed! We'll Get Em Next Time");
-            })
-            .catch((error) => {
-                console.error('Error:', error);
+            body: JSON.stringify({ login: this.state.login, password: this.state.password }),
+        })
+        .then((response) => {
+            // faking a happy response
+            alert(this.isDataValid(this.state.login, this.state.password) ? 'Success! Congratulations' : 'Mission Failed! We\\´ll Get Em Next Time');
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
     }
 
     render() {
-        return (<div className="login">
+        return (<div className='login'>
             <Box
-                component="form"
-                id="form"
+                component='form'
+                id='form'
                 sx={{
                     width: 500,
                     maxWidth: '100%',
                 }}
                 noValidate
-                autoComplete="off"
+                autoComplete='off'
             >
                 <div>
                     <TextField
-                        size="small"
-                        fullWidth 
-                        id="login"
-                        label="Login"
-                        margin="dense"
+                        size='small'
+                        fullWidth
+                        id='login'
+                        label='Login'
+                        margin='dense'
                         value={this.state.login}
                         color={this.getColor(this.state.login, 'login')}
-                        error={this.getError(this.state.login, "login") !== ""}
-                        onChange={(e) => this.setState({login : e.target.value})}
-                        helperText={this.getError(this.state.login, "login")}
+                        error={this.getError(this.state.login, 'login') !== ''}
+                        onChange={(e) => this.setState({ login: e.target.value })}
+                        helperText={this.getError(this.state.login, 'login')}
                     />
                 </div>
                 <div>
-                    <FormControl variant="outlined"
-                                 margin="dense"
-                                 size="small"
-                                 error={this.getError(this.state.password, "password") !== ""}
-                                 color={this.getColor(this.state.password, 'password')}
-                                 fullWidth>
-                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <FormControl variant='outlined'
+                        margin='dense'
+                        size='small'
+                        error={this.getError(this.state.password, 'password') !== ''}
+                        color={this.getColor(this.state.password, 'password')}
+                        fullWidth>
+                        <InputLabel htmlFor='outlined-adornment-password'>Password</InputLabel>
                         <OutlinedInput
-                            id="outlined-adornment-password"
+                            id='outlined-adornment-password'
                             type={this.state.showPassword ? 'text' : 'password'}
                             value={this.state.password}
-                            onChange={(e) => this.setState({password : e.target.value})}
+                            onChange={(e) => this.setState({ password: e.target.value })}
                             endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={this.handleClickShowPassword}
-                                    edge="end"
-                                >
-                                {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
+                                <InputAdornment position='end'>
+                                    <IconButton
+                                        aria-label='toggle password visibility'
+                                        onClick={this.handleClickShowPassword}
+                                        edge='end'
+                                    >
+                                        {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
                             }
-                            label="Password"
+                            label='Password'
                         />
-                        {this.getError(this.state.password, "password") !== "" && <FormHelperText error id="component-helper-text">
-                            {this.getError(this.state.password, "password")}
+                        {this.getError(this.state.password, 'password') !== '' && <FormHelperText error id='component-helper-text'>
+                            {this.getError(this.state.password, 'password')}
                         </FormHelperText>}
                     </FormControl>
                 </div>
                 <div className='button'>
-                    <Button 
-                        variant="contained"
+                    <Button
+                        variant='contained'
                         endIcon={<SendIcon />}
                         onClick={(e) => this.sendData()}
                         disabled={!this.isDataValid(this.state.login, this.state.password)}
@@ -149,7 +149,7 @@ class Login extends Component<IProps,  IState> {
                 </div>
             </Box>
         </div>
-    )
+        )
     }
 }
 
